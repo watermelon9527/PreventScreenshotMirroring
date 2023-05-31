7da952812bb385116e5b6726a914f23f5997b381
chijung chan <nono850226@gmail.com>
2023-05-31 14:53:23 +0800

    Using UIView extension to mask view.

--- textField/ViewController.swift
+++ textField/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.isSecureTextEntry = true
+        textField.makeSecure()
+        
     }
 
 
 }
-
+extension UIView {
+    func makeSecure() {
+        DispatchQueue.main.async {
+            let field = UITextField()
+            field.isSecureTextEntry = true
+            self.addSubview(field)
+            field.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+            field.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+            self.layer.superlayer?.addSublayer(field.layer)
+            field.layer.sublayers?.first?.addSublayer(self.layer)
+        }
+    }
+}
